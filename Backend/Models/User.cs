c59716e023ab5c6a81646b5c3144cd867704ead7
gabi <gabika.hun@gmail.com>
2023-07-17 14:08:06 +0200

    added post and user cs files

---
+++ Backend/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Backend.Models
+{
+    public class User : IdentityUser
+    {
+        public string Bio { get; set; }
+        public string ProfilePictureUrl { get; set; }
+
+        // navigation properties 
+        public virtual ICollection<Post> Posts { get; set; }
+
+        public User()
+        {
+            Posts = new HashSet<Post>();
+        }
+    }
+}
