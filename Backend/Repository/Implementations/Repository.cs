0d22e01ed17bff33fac8f67020921b62a1e8ab3e
gabi <gabika.hun@gmail.com>
2023-07-17 15:24:16 +0200

    added base repository class

---
+++ Backend/Repository/Implementations/Repository.cs
@@ -0,0 +1,37 @@
+﻿using Backend.Repository.Interfaces;
+
+namespace Backend.Repository.Implementations
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Delete(string uId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(string uId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Backend/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Repository.Interfaces
+{
+    public interface IRepository <T>
+    {
+        void Create(T entity);
+        T Get (string uId);
+        IQueryable<T> GetAll ();
+        void Update (T entity);
+        T Delete (string uId);
+        void SaveChanges();
+    }
+}
