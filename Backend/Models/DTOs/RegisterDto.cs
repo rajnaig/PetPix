39a3490c49b32646afeb5a3dfaa05f8c77ac4a13
Páli Dávid <pali.david2001@outlook.com>
2023-07-18 21:27:08 +0200

    Auth 1.st

---
+++ Backend/Models/DTOs/RegisterDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.DTOs
+{
+    public class RegisterDto
+    {
+        [Required] public string Username { get; set; }
+
+        [Required] public string Email { get; set; }
+
+
+        [Required]
+        [StringLength(10)]
+        public string Password { get; set; }
+    }
+}
---
+++ Backend/Services/ServiceImplementations/TokenGeneratorService.cs
@@ -0,0 +1,50 @@
+﻿using Backend.Models;
+using Backend.Services.ServiceInterfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Backend.Services.ServiceImplementations
+{
+    public class TokenGeneratorService : ITokenGeneratorService
+    {
+        private readonly SymmetricSecurityKey _symmetricSecurityKey;
+        private readonly UserManager<User> _userManager;
+
+        public TokenGeneratorService(IConfiguration config, UserManager<User> userManager)
+        {
+            _symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+            _userManager = userManager; 
+        }
+
+
+        public async Task<string> GenerateTokenAsync(User user)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName),
+
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+
+            var creds = new SigningCredentials(_symmetricSecurityKey, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
